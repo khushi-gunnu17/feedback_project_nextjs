56ed1bf8fda25d7104acf5d111b68db784e39c15
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-07-17 12:19:11 +0530

    adding pages and delete backend

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,118 @@
+"use client"
+
+import {
+    Card,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "./ui/button"
+import { X } from "lucide-react"
+import { Message } from "@/models/user.models"
+import { useToast } from "./ui/use-toast"
+import { ApiResponse } from "@/types/apiResponse"
+import axios, {AxiosError} from "axios"
+import dayjs from 'dayjs';
+
+
+type MessageCardProps = {
+    message : Message
+    onMessageDelete : (messageId : string) => void 
+}
+
+
+const MessageCard = ({message, onMessageDelete} : MessageCardProps) => {
+
+    const {toast} = useToast()
+
+
+    const handleDeleteConfirm = async() => {
+
+        try {
+
+            const response = await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+    
+            toast({
+                title : response.data.message
+            })
+    
+            onMessageDelete(message._id as string)        // error here coz of type checking
+
+        } catch (error) {
+
+            const axiosError = error as AxiosError<ApiResponse>
+
+            toast({
+                title : "Error",
+                description : axiosError.response?.data.message ?? "Failed to delete message.",
+                variant : "destructive"
+            })
+            
+        }
+
+    }
+
+
+    return (
+
+        <Card>
+            <CardHeader>
+                
+                <div className="flex justify-between items-center">
+                
+                    <CardTitle>{message.content}</CardTitle>
+
+                    {/* Alert Dialog Box */}
+                    <AlertDialog>
+
+                        <AlertDialogTrigger asChild>
+                            <Button variant="destructive"><X className="w-5 h-5"/></Button>
+                        </AlertDialogTrigger>
+
+                        <AlertDialogContent>
+
+                            <AlertDialogHeader>
+
+                                <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+
+                                <AlertDialogDescription>
+                                    This action cannot be undone. This will permanently delete your
+                                    account and remove your data from our servers.
+                                </AlertDialogDescription>
+
+                            </AlertDialogHeader>
+
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction onClick={handleDeleteConfirm}>Continue</AlertDialogAction>
+                            </AlertDialogFooter>
+                            
+                        </AlertDialogContent>
+
+                    </AlertDialog>
+                </div>
+
+                <div className="text-sm">
+                    {dayjs(message.createdAt).format('MMM D, YYYY h:mm A')}
+                </div>
+
+                
+            </CardHeader>
+
+        </Card>
+
+    )
+}
+
+export default MessageCard
