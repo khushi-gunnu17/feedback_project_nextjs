0c85c18b2bb960bf8d5d5b06a4ec32d7a4bfef07
khushi-gunnu17 <khushiastrogeek@gmail.com>
2024-07-17 14:29:06 +0530

    adding dashboard page

--- src/types/apiResponse.ts
+++ src/types/apiResponse.ts
@@ -3,7 +3,7 @@ import { Message } from "@/models/user.models"
 export interface ApiResponse {
     success : boolean
     message : string
-    isAcceptingMessages? : boolean
+    isAcceptingMessage? : boolean
     messages? : Array<Message>
 }
 
